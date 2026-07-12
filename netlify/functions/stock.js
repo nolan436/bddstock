cc49bc4e902b4261651da5192c1e154ef0398c30
nolan436 <nono.chome@gmail.com>
2026-07-12 14:06:16 +0200

    Implement GET and POST handlers for stock data

--- netlify/functions/stock.js
+++ netlify/functions/stock.js
@@ -1,1 +1,26 @@
+const { getStore } = require("@netlify/blobs");
 
+exports.handler = async (event) => {
+    const store = getStore("resellflow_store");
+    
+    if (event.httpMethod === "GET") {
+        const data = await store.get("stock", { type: "json" });
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data || [])
+        };
+    }
+    
+    if (event.httpMethod === "POST") {
+        const stockData = JSON.parse(event.body);
+        await store.setJSON("stock", stockData);
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ success: true })
+        };
+    }
+    
+    return { statusCode: 405 };
+};
