e32d13e1bdaba07c0aa23cc8818dd70d99df795a
nolan436 <nono.chome@gmail.com>
2026-07-12 14:22:14 +0200

    Add CORS headers to stock function response

--- netlify/functions/stock.js
+++ netlify/functions/stock.js
@@ -12,11 +12,14 @@ exports.handler = async (event) => {
             try {
                 data = await store.get("stock", { type: "json" });
             } catch (e) {
-            
+                // Si le store est vide
             }
             return {
                 statusCode: 200,
-                headers: { "Content-Type": "application/json" },
+                headers: { 
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                },
                 body: JSON.stringify(data || [])
             };
         }
